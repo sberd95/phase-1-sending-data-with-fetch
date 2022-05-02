a6024d6cb2d1c60e8dff0129e4adf9bb1d8d28f8
Sergio Berdeja <sergioberdeja@gmail.com>
2022-05-02 11:54:47 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body:JSON.stringify({'name':userName, 'email':userEmail})
+        })
+        .then(response => response.json())
+        .then(object => document.querySelector('body').append(object.id))
+        .catch(object => document.querySelector('body').append(object.message))
+}
